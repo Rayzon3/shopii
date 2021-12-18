18e17e29f5ac8bcfc1a548233e09fbfea67b5b06
Rahul Bhardwaj <rahul.03111999@gmail.com>
2021-12-18 15:27:12 +0530

    pushed

---
+++ src/entities/User.ts
@@ -0,0 +1,52 @@
+import { IsEmail, Length } from "class-validator";
+import {Entity, PrimaryGeneratedColumn, Column, BaseEntity, Index, CreateDateColumn, UpdateDateColumn, BeforeInsert} from "typeorm";
+import bcrypt from "bcrypt"
+import { classToPlain, Exclude } from "class-transformer"
+
+@Entity("users")
+export class User extends BaseEntity {
+
+    constructor(user: Partial<User>){
+        super()
+        Object.assign(this, user)
+    }
+
+    @PrimaryGeneratedColumn()
+    @Exclude()
+    id: number;
+
+    @Index()
+    @Length(3, 255)
+    @Column({ unique: true })
+    username: string;
+
+    @Index()
+    @IsEmail()
+    @Column({ unique: true })
+    email: string;
+
+  
+    @Column()
+    @Length(6, 255)
+    @Exclude()
+    password: string;
+
+    @Index()
+    @Column({ nullable: true })
+    address: string;
+
+    @CreateDateColumn()
+    createdAt: Date
+
+    @UpdateDateColumn()
+    updatedAt: Date
+
+    @BeforeInsert()
+    async hashPassword(){
+        this.password = await bcrypt.hash(this.password, 6)
+    }
+
+    toJSON(){
+        return classToPlain(this)
+    }
+}
