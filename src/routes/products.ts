2ae9d1db32d7d881118c98293fec3c7515c58c60
Rahul Bhardwaj <rahul.03111999@gmail.com>
2021-12-28 15:57:23 +0530

    added product table and routes

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from "express";
 import Product from "../entities/Product";
+import multer from "multer"
 
 import auth from "../middleware/auth"
 
@@ -22,8 +23,42 @@ const createProduct = async (req: Request, res: Response) => {
     }
 }
 
+//for product feed
+const getProducts = async (req: Request, res: Response) => {
+    try{
+        const products = await Product.find({
+            order: { createdAt: "DESC" }
+        })
+        return res.json(products)
+    }catch(e){
+        console.log(e);
+        return res.json({ error: "Oops! That was not supposed to happen :/" })
+    }
+}
+
+//for single product
+const getProduct = async (req: Request, res: Response) => {
+    const { identifier, slug } = req.params  
+    try{
+        const product = await Product.findOneOrFail({
+            identifier, slug
+        })
+        return res.status(500).json(product)
+    }catch(e){
+        console.log(e);
+        return res.status(404).json({ error: "Product not found :/" })
+    }
+}
+
+const uploadProductImage = async (req: Request, res: Response) => {
+
+}
+
+
 const router = Router()
 
 router.post("/", auth, createProduct)
+router.get("/", getProducts)
+router.get("/:identifier/:slug", getProduct)
 
 export default router
