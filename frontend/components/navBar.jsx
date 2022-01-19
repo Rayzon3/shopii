1aa1f58c71110bfd0f6693f7110154398ae30818
Shivam <65867433+shivam-2311@users.noreply.github.com>
2022-01-19 19:25:53 +0530

    login

--- frontend/components/navBar.jsx
+++ frontend/components/navBar.jsx
@@ -1,6 +1,16 @@
 import { Disclosure } from '@headlessui/react'
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { logout } from '../redux/userRedux';
 
 export default function NavBar() {
+  const user = useSelector((state) => state.user.user);
+  const dispatch = useDispatch();
+  // const handlelogout = () => {
+  //   dispatch(logout(null));
+  //   console.log(user);
+  // }
+
   return (
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
@@ -11,12 +21,22 @@ export default function NavBar() {
               </div>
               <div className="flex items-center justify-center flex-1 sm:items-stretch sm:justify-start">
                 <div className="flex items-center flex-shrink-0">
-                    <h2 className="text-green">[Site Name + svg Logo]</h2>
+                  <h2 className="text-green">[Site Name + svg Logo] </h2>
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 space-x-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                  <a href="/login"><button className="px-3 py-1 rounded bg-green transform motion-safe:hover:scale-110 ..." >Login</button></a>
-                  <a href="/signup"><button className="px-3 py-1 rounded bg-green transform motion-safe:hover:scale-110 ...">Sign Up</button></a>
+                {
+                  user ?
+                    <>
+                      <h2 className="text-3xl text-white">{user.username}</h2>
+                      <button className="px-3 py-1 rounded bg-green transform motion-safe:hover:scale-110 ..." onSubmit={handlelogout} >Logout</button>
+                    </>
+                    :
+                    <>
+                      <a href="/login"><button className="px-3 py-1 rounded bg-green transform motion-safe:hover:scale-110 ...">LogIn</button></a>
+                      <a href="/signup"><button className="px-3 py-1 rounded bg-green transform motion-safe:hover:scale-110 ...">Sign Up</button></a>
+                    </>
+                }
               </div>
             </div>
           </div>
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,60 +1,20 @@
 import Head from 'next/head'
+import { useSelector } from 'react-redux'
 import NavBar from '../components/navBar.jsx'
+import Main from '../components/Home.jsx'
 
 export default function Home() {
 
   
   return (
-    <div className="flex flex-col min-h-screen py-2 bg-midNight">
+    <div >
       <Head>
         <title>[Site Name]</title>
         <link rel="icon" href="/tree.ico" />
       </Head>
-      <NavBar />
-      <main className="flex flex-col items-center justify-center flex-1 w-full px-10 text-center text-white">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-green" href="/">
-            [Site Name]!
-          </a>
-        </h1>
+  
+     <Main/>
 
-        <p className="mt-3 text-2xl">
-          Your One Stop To
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="/buy"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-green focus:text-green"
-          >
-            <h3 className="text-2xl font-bold">Buy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              We got everthing you need!
-            </p>
-          </a>
-
-          <a
-            href="/sell"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-green focus:text-green"
-          >
-            <h3 className="text-2xl font-bold">Sell &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Sell your stuff at great prices!
-            </p>
-          </a>
-
-          <a
-            href="#"
-            className="p-6 mt-6 text-left transform border w-96 rounded-xl hover:text-green focus:text-green"
-          >
-            <h3 className="text-2xl font-bold">Recycle &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Save the environment!
-            </p>
-          </a>
-        </div>
-      </main>
     </div>
   )
 }
