eea052b829214153fcd532a507459708c9affb0c
Rahul Bhardwaj <rahul.03111999@gmail.com>
2022-01-20 16:00:20 +0530

    upload images

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,96 +1,108 @@
 import { Router, Request, Response } from "express";
 import Product from "../entities/Product";
-import multer, { FileFilterCallback } from "multer"
-import path from "path"
+import multer, { FileFilterCallback } from "multer";
+import path from "path";
 
-import auth from "../middleware/auth"
+import auth from "../middleware/auth";
 import makeId from "../util/helpers";
 
 const createProduct = async (req: Request, res: Response) => {
-    const { title, description, price } = req.body
+  const { title, description, price } = req.body;
 
-    const user = res.locals.user
-    if(title.trim() === ""){
-        res.status(400).json({ title: "Product title must not be empty!!" })
-    }
+  const user = res.locals.user;
+  if (title.trim() === "") {
+    res.status(400).json({ title: "Product title must not be empty!!" });
+  }
 
-    try{
-        const product = new Product({ title, description, user, price })
-        await product.save()
+  try {
+    const product = new Product({ title, description, user, price });
+    // upload.single("file");
+    // product.imageUrn = req.file.filename;
 
-        return res.json(product)
-    }catch(e){
-        console.log(e)
-        return res.status(500).json({ error: "Oops! Something went wrong." })
-    }
-}
+    await product.save();
+
+    return res.json(product);
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ error: "Oops! Something went wrong." });
+  }
+};
 
 //for product feed
 const getProducts = async (req: Request, res: Response) => {
-    try{
-        const products = await Product.find({
-            order: { createdAt: "DESC" }
-        })
-        return res.json(products)
-    }catch(e){
-        console.log(e);
-        return res.json({ error: "Oops! That was not supposed to happen :/" })
-    }
-}
+  try {
+    const products = await Product.find({
+      order: { createdAt: "DESC" },
+    });
+    return res.json(products);
+  } catch (e) {
+    console.log(e);
+    return res.json({ error: "Oops! That was not supposed to happen :/" });
+  }
+};
 
 //for single product
 const getProduct = async (req: Request, res: Response) => {
-    const { identifier, slug } = req.params  
-    try{
-        const product = await Product.findOneOrFail({
-            identifier, slug
-        })
-        return res.status(500).json(product)
-    }catch(e){
-        console.log(e);
-        return res.status(404).json({ error: "Product not found :/" })
-    }
-}
+  const { identifier, slug } = req.params;
+  try {
+    const product = await Product.findOneOrFail({
+      identifier,
+      slug,
+    });
+    return res.status(500).json(product);
+  } catch (e) {
+    console.log(e);
+    return res.status(404).json({ error: "Product not found :/" });
+  }
+};
 
 const upload = multer({
-    storage: multer.diskStorage({
-        destination: "public/images",
-        filename: (req, file, callback) => {
-            const name = makeId(15)
-            callback(null, name + path.extname(file.originalname)) // will look like this kdhsbvvwkjvhlw + .png
-        }
-    }),
-    fileFilter: (_, file: any, callback: FileFilterCallback) => {
-        console.log(file)
-        if(file.mimetype == 'image/jpeg' || file.mimetype == 'image/png'){
-            callback(null, true)
-        }else{
-            callback(new Error("Wrong file type!!"))
-        }
+  storage: multer.diskStorage({
+    destination: "public/images",
+    filename: (req, file, callback) => {
+      const name = makeId(15);
+      callback(null, name + path.extname(file.originalname)); // will look like this kdhsbvvwkjvhlw + .png
+    },
+  }),
+  fileFilter: (_, file: any, callback: FileFilterCallback) => {
+    console.log(file);
+    if (file.mimetype == "image/jpeg" || file.mimetype == "image/png") {
+      callback(null, true);
+    } else {
+      callback(new Error("Wrong file type!!"));
     }
-})
+  },
+});
+
 const uploadProductImage = async (req: Request, res: Response) => {
-    const product: Product = res.locals.product
-    try{
-        const type = req.body.type
-        if(type !== "image"){
-            return res.status(400).json({ error: "Wrong file type!!" })
-        }
-        product.imageUrn = req.file.filename
-        await product.save()
-    }catch(e){
-        console.log(e)
-        return res.status(500).json({ error: "Something went wrong!!" })
+  const { identifier, slug } = req.params;
+  try {
+    const product = await Product.findOneOrFail({
+      identifier,
+      slug,
+    });
+    const type = req.body.type;
+    if (type !== "image") {
+      return res.status(400).json({ error: "Wrong file type!!" });
     }
-    res.json({ success: true })
-}
+    product.imageUrn = req.file.filename;
+    await product.save();
+    return res.json(product);
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ error: "Something went wrong!!" });
+  }
+};
 
+const router = Router();
+router.post("/", auth, createProduct);
+router.post(
+  "/:identifier/:slug/image",
+  auth,
+  upload.single("file"),
+  uploadProductImage
+);
+router.get("/", getProducts);
+router.get("/:identifier/:slug", getProduct);
 
-const router = Router()
-
-router.post("/", auth, createProduct)
-router.post("/image", auth, upload.single('file'), uploadProductImage)
-router.get("/", getProducts)
-router.get("/:identifier/:slug", getProduct)
-
-export default router
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,38 +1,38 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
+import { createConnection } from "typeorm";
 import express from "express";
 import morgan from "morgan";
-import dotenv from "dotenv"
-import cookieParser from "cookie-parser"
-import cors from 'cors'
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-dotenv.config()
+dotenv.config();
 
-import authRoutes from "./routes/auth"
-import productRoutes from "./routes/products"
+import authRoutes from "./routes/auth";
+import productRoutes from "./routes/products";
 
-import trim from "./middleware/trim"
+import trim from "./middleware/trim";
 
-const app = express()
-const PORT = process.env.PORT
+const app = express();
+const PORT = process.env.PORT;
 
-app.use(cors())
-app.use(express.json())
-app.use(morgan("dev"))
-app.use(trim)
-app.use(cookieParser())
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(trim);
+app.use(cookieParser());
 
-app.get("/", (req, res) => res.send("Hello World!"))
-app.use("/api/auth", authRoutes)
-app.use("/api/products", productRoutes)
+app.get("/", (req, res) => res.send("Hello World!"));
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, async () => {
-    console.log(`Server running at https://localhost:${PORT}`)
+  console.log(`Server running at https://localhost:${PORT}`);
 
-    try{
-        await createConnection()
-        console.log("Database Connected !!")
-    }catch(e){
-        console.log(e)
-    }
-})
+  try {
+    await createConnection();
+    console.log("Database Connected !!");
+  } catch (e) {
+    console.log(e);
+  }
+});
